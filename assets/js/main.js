2eadcba43592c7a0621445c91b7f068f0c8993ef
SuperCoder <supercoder@MacBook-Air-von-SuperCoder-2.local>
2022-01-18 13:49:58 +0100

    comm

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+// ------ Lev1_2 -------
+
+document.getElementById("info").innerHTML = '<h1>Hello World</h1> <br> <h2>How are you?</h2>';
+document.getElementById('container').innerHTML = '<p>start of the element</p>';
+document.write('end of the element');
